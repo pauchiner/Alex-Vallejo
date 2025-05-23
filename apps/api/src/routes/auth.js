6b8d177357c31ee8f2f525c79d348ce55623f33a
Pau García Chiner <pauchiner@Paus-MacBook-Air.local>
2025-05-23 10:10:11 +0200

    fix: unusedf file

--- apps/api/src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import {auth} from '../lib/auth';
-import {Hono} from 'hono';
-
-const authentication = new Hono();
-
-authentication.on(['POST', 'GET'], '**', c => auth.handler(c.req.raw));
-
-export {authentication};
