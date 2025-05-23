43e4464d60bc6b2d639eb02fb806c34bd37db323
Pau García Chiner <pauchiner@Paus-MacBook-Air.local>
2025-05-23 11:41:56 +0200

    feat: implement calendar api route

--- apps/api/src/routes/calendar.js
+++ apps/api/src/routes/calendar.js
@@ -1,7 +1,7 @@
 import {Hono} from 'hono';
 import {auth} from '../lib/auth';
 
-import {database} from '../lib/database';
+import {database, schema} from '../lib/database';
 
 const calendar = new Hono();
 
@@ -10,15 +10,37 @@ calendar.get('/', async c => {
     headers: c.req.raw.headers
   });
 
-  if (!authorized || !authorized.session || authorized.user) {
-    return c.json({error: 'No estás logeado!!!!)'}, 401);
+  if (!authorized) {
+    return c.json({error: 'Inicia sesión para continuar.'}, 401);
   }
 
-  const resultado = await database.query.calendar.findMany({});
+  const query = await database.query.calendar.findMany({});
 
-  if (resultado.length === 0) {
-    return c.json({error: 'NO HAY EVENTOS'});
+  if (query.length === 0) {
+    return c.json([]);
   }
+
+  const result = query.map(event => {
+    return {
+      id: event.id,
+      title: event.title,
+      start: event.start_date,
+      ubicacion: event.location,
+      descripcion: event.description
+    }
+  })
+
+  return c.json(result);
+
+  /*
+  return c.json([
+    {
+      title: "Día de cine",
+      start: "2025-04-21",
+      end: "2025-04-25",
+      ubicacion: "",
+      descripcion: "",
+    },
 
   return c.json(resultado);
 
@@ -55,6 +77,18 @@ calendar.get('/', async c => {
 });
 
 calendar.post('/', async c => {
+  const authorized = await auth.api.getSession({
+    headers: c.req.raw.headers
+  });
+
+  if (!authorized) {
+    return c.json({error: 'Inicia sesión para continuar.'}, 401);
+  }
+
+  if(authorized.user.role !== 'admin') {
+    return c.json({error: 'No tienes permisos para hacer esta acción.'}, 401);
+  }
+
   const nuevoEvento = await c.req.json();
 
   // aqui valida que los campos del object esten bien
@@ -63,8 +97,13 @@ calendar.post('/', async c => {
   // Aquí guardas en una base de datos
   database
     .insert(schema.calendar)
-    .values(nuevoEvento)
-    .then(data => console.log(data));
+    .values({
+      title: nuevoEvento.titulo ?? "",
+      location: nuevoEvento.ubicacion ?? "",
+      start_date: nuevoEvento.fecha ?? Date.now().toString(),
+      hour: nuevoEvento.hora ?? "",
+      description: nuevoEvento.descripcion ?? "",
+    })
 
   return c.json({
     success: true,
